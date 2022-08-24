07dfd7992f3908c6321cb6153e791a4086b467dd
김경석 <0xe82de@gmail.com>
2022-08-24 12:19:58 +0900

    #3 feat: auditor provider 빈 등록

--- src/main/java/com/cheezeburger/oauth/OauthApplication.java
+++ src/main/java/com/cheezeburger/oauth/OauthApplication.java
@@ -1,7 +1,12 @@
 package com.cheezeburger.oauth;
 
+import com.cheezeburger.oauth.global.config.properties.AppProperties;
+import com.cheezeburger.oauth.global.config.properties.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.Optional;
@@ -17,5 +22,10 @@ public class OauthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OauthApplication.class, args);
+	}
+
+	@Bean
+	public AuditorAware<String> auditorProvider() {
+		return () -> Optional.of(UUID.randomUUID().toString());
 	}
 }
